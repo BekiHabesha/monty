ac01379606f8949c03d171d5f08c3ea0d0aac18e
BekiHabesha <bereketdere@gmail.com>
2023-12-14 02:38:51 +0300

    f_nop.c - monty Project.

---
+++ f_nop.c
@@ -0,0 +1,38 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 f_nop.c file
+ *                     Task Type: 🟥 Mandatory Task
+ *                   Task Number: 🔢 No 5
+ *                     Functions: 💻 void f_nop
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ *f_nop- nothing
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: stack head
+ * @counter: line_number
+ *
+ * Return: no return
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
