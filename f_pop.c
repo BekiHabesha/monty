a46bbfb339e270b8a6682b8ee72577f4db9d1fc3
BekiHabesha <bereketdere@gmail.com>
2023-12-14 02:47:13 +0300

    f_pop.c - monty Project.

---
+++ f_pop.c
@@ -0,0 +1,49 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 f_pop.c file
+ *                     Task Type: 🟥 Mandatory Task
+ *                   Task Number: 🔢 No 2
+ *                     Functions: 💻 void f_pop
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ * f_pop - It prints the top
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
