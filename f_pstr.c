7de43a65ef3c5ae9b9c34a0c591b31ae786dc210
BekiHabesha <bereketdere@gmail.com>
2023-12-14 02:49:30 +0300

    f_pstr.c - monty Project.

---
+++ f_pstr.c
@@ -0,0 +1,51 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 f_pstr.c file
+ *                     Task Type: 📘 Advanced Task
+ *                   Task Number: 🔢 No 12
+ *                     Functions: 💻 void f_pstr
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ * f_pstr - It prints string starting at the top
+ *          of the stack,followed by a new
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
