00c0ef206fd7bd1dce845dbfb0f547e9955c2b26
BekiHabesha <bereketdere@gmail.com>
2023-12-14 02:45:20 +0300

    f_pint.c - monty Project.

---
+++ f_pint.c
@@ -0,0 +1,45 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 f_pint.c file
+ *                     Task Type: 🟥 Mandatory Task
+ *                   Task Number: 🔢 No 1
+ *                     Functions: 💻 void f_pint
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ * f_pint - It prints the top
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
