fb381b32b16830873da162aeedbf1b9a33ba8e0d
BekiHabesha <bereketdere@gmail.com>
2023-12-14 02:16:42 +0300

    add_node.c - monty Project.

---
+++ add_node.c
@@ -0,0 +1,51 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 add_node.c file
+ *                     Task Type: 📘 Advanced Task
+ *                   Task Number: 🔢 No
+ *                     Functions: 💻 void addnode
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ * addnode - It add node to the head stack
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The head of the stack
+ * @n: The new_value
+ *
+ * Return: no return
+ */
+
+void addnode(stack_t **head, int n)
+{
+
+	stack_t *new_node, *aux;
+
+	aux = *head;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (aux)
+		aux->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
