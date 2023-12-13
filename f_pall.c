178e4cc35ce6d5d4143e0bfed38ed759dc823414
BekiHabesha <bereketdere@gmail.com>
2023-12-14 02:40:49 +0300

    f_pall.c - monty Project.

---
+++ f_pall.c
@@ -0,0 +1,48 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 f_pall.c file
+ *                     Task Type: 🟥 Mandatory Task
+ *                   Task Number: 🔢 No 0
+ *                     Functions: 💻 void f_pall
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ * f_pall - It prints the stack
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ *
+ * @counter: It have no used
+ *
+ * Return: no return
+ */
+
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
