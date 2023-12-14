a5b2b62272b7a79002eadca23376983ff5281dd1
BekiHabesha <bereketdere@gmail.com>
2023-12-14 16:47:46 +0300

    f_rotr.c - monty Project.

---
+++ f_rotr.c
@@ -0,0 +1,52 @@
+/*
+ * Project Description:📝
+ *                               Project Weight: 2 (Two)
+ *                           Project Start Date: 📅Dec 12, 2023 🕕6:00 AM
+ *                             Project End Date: 📅Dec 15, 2023 🕕6:00 AM
+ *                        Checker Released Date: 📅Dec 13, 2023 🕛12:00 AM
+ *                    Auto Review Launched Date: 📅@Deadline(Dec 15, 2023)
+ *
+ * File Details: 📁📖
+ *                     File_name: 📋 f_rotr.c file
+ *                     Task Type: 📘 Advanced Task
+ *                   Task Number: 🔢 No 14
+ *                     Functions: 💻 void f_rotr
+ *                 Project Title: 📒 0x19.C-Stacks, Queues-LIFO, FIFO
+ *             GitHub Repository: 📦 monty
+ *                     Directory: 📁 monty and bf
+ *                       Created: 📑 On 📅 December 14, 2023
+ *                        Author: 🖍️💻 Bereket Dereje Mekkonen👨
+ */
+
+#include "monty.h"
+
+/**
+ *f_rotr- It rotates the stack to the bottom
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
